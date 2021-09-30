db92772db6ed696c91c079c02c7e70ebd3773bb2
HieuPT <hieuptse130099@fpt.edu.vn>
2021-09-30 17:16:27 +0700

    [Mobile][UIC-13][SCSS-82] Add Profile UI

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,29 @@
+part of 'constants.dart';
+
+class AppColors {
+  // Green
+  static const Color greenFF61C53D = Color(0xFF61C53D);
+  static const Color greenFF66D095 = Color(0xFF66D095);
+  static const Color greenFF39AC8F = Color(0xFF39AC8F);
+
+  // Grey
+  static const Color greyFF9098B1 = Color(0xFF9098B1);
+  static const Color greyFFDADADA = Color(0xFFDADADA);
+  static const Color greyFFEEEEEE = Color(0xFFEEEEEE);
+
+  //Orange
+  static const Color orangeFFF5670A = Color(0xFFF5670A);
+
+  //White
+  static const Color white = Colors.white;
+
+  //Black
+  static const Color black = Colors.black;
+  static const Color blackBB000000 = Color(0xBB000000);
+
+  //Red
+  static const Color red = Colors.red;
+
+  //Yellow
+  static const Color yellowFFF3F09A = Color(0xFFF3F09A);
+}
