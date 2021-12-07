0c9c8e6927d17304317576c16c4d16247aa946db
Tien <tienjtran@gmail.com>
2021-12-07 17:21:10 +0700

    add to production main

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,16 +1,41 @@
+import 'package:collector_app/constants/constants.dart';
 import 'package:collector_app/providers/configs/injection_config.dart';
 import 'package:collector_app/providers/services/firebase_service.dart';
 import 'package:collector_app/ui/app.dart';
 import 'package:collector_app/utils/env_util.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 void main() async {
+  // Load env
   await dotenv.load(fileName: EnvAppSetting.production);
-
   configureDependencies();
   final firebase = getIt.get<FirebaseNotification>();
   await firebase.initialize();
-  print(await firebase.getToken());
+
+  await configLoading();
   runApp(CollectorApp());
+}
+
+Future<void> configLoading() async {
+  EasyLoading.instance
+    ..loadingStyle = EasyLoadingStyle.custom
+    ..backgroundColor = Colors.white
+    ..indicatorColor = Colors.green
+    ..textColor = Colors.green
+    ..maskType = EasyLoadingMaskType.black
+    ..indicatorWidget = Stack(
+      alignment: Alignment.center,
+      children: [
+        SizedBox(width: 60, height: 60, child: CircularProgressIndicator()),
+        Image.asset(
+          ImagesPaths.collectorLogo,
+          height: 40,
+          width: 40,
+        ),
+      ],
+    )
+    ..userInteractions = false
+    ..dismissOnTap = false;
 }
